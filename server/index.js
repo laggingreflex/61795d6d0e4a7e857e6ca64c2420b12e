b1146e5e6249c42c4a766140812571d42c33c52b
freethlua <freethlua@gmail.com>
2017-05-04 14:26:03 +0530

    update-addedmoreroutes

--- server/index.js
+++ server/index.js
@@ -19,17 +19,73 @@ router.post('/shop', async ctx => {
   ctx.body = { id: shop.id };
 });
 
-router.post('/shop/:id/product', async ctx => {
-  const shop_id = ctx.params.id;
+router.post('/shop/:shop_id/product', async ctx => {
+  const shop_id = ctx.params.shop_id;
   const product_name = ctx.body.name;
 
   const shop = await ShopDB.getShopBtId(shop_id);
   const product = shop.addProduct({ name: product_name })
 
   ctx.body = { id: product.id };
+
+  (() => {
+    // async op; no need to block request while incrementing the shop requests field
+    shop.requests++;
+    shop.save();
+  })
 });
 
 
+router.put('/shop/:shop_id/product/:product_id', async ctx => {
+  const shop_id = ctx.params.shop_id;
+  const product_id = ctx.params.product_id;
+  const product = ctx.body;
+
+  const shop = await ShopDB.getShopBtId(shop_id);
+  await shop.findAndUpdateProduct({ id: product_id, ...product }); // sanitization etc expected to be handled in DB wrapper
+
+  ctx.body = { id: product.id };
+
+  (() => {
+    // async op; no need to block request while incrementing the shop requests field
+    shop.requests++;
+    shop.save();
+  })
+});
+
+
+router.delete('/shop/:shop_id/product/:product_id', async ctx => {
+  const shop_id = ctx.params.shop_id;
+  const product_id = ctx.params.product_id;
+
+  const shop = await ShopDB.getShopBtId(shop_id);
+  await shop.findAndRemoveProduct({ id: product_id });
+
+  ctx.body = { id: product_id };
+
+  (() => {
+    // async op; no need to block request while incrementing the shop requests field
+    shop.requests++;
+    shop.save();
+  })
+});
+
+
+
+router.get('/shop/:shop_id/products', async ctx => {
+  const shop_id = ctx.params.shop_id;
+
+  const shop = await ShopDB.getShopBtId(shop_id);
+  const products = await shop.getAllProducts();
+
+  ctx.body = { products };
+
+  (() => {
+    // async op; no need to block request while incrementing the shop requests field
+    shop.requests++;
+    shop.save();
+  })
+});
 
 
 server.use(bodyparser())
